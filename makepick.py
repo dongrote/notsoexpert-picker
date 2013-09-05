cf450d3026cd9c70d8d11e022d7fa503f8497bba
Don Grote <dgrote@tacnetsol.com>
2013-09-05 10:32:33 -0400

    Initial implementation.
    Initial implementation of picker AI.

---
+++ makepick.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import random
+import time
+
+
+def main():
+    random.seed(time.time())
+    score = random.random()
+    if score > 0.5:
+        print "AWAY"
+    else:
+        print "HOME"
+
+
+if '__main__' == __name__:
+    main()
